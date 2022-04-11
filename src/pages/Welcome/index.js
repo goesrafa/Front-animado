cb559584f2db73fc46093fafebb85972eee51020
goesrafa <rafa.goes.2012@gmail.com>
2022-04-11 10:26:08 -0300

    Front Inicial

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Welcome() {
+    return(
+        <View>
+            <Text>Seja bem vindo ao EasyWay</Text>
+        </View>
+    )
+}
